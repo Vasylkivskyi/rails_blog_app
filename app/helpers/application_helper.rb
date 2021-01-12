91f3e0972f8af1dc9427d7fcf9437f659d42cb6c
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2021-01-12 09:36:14 +0200

    add user show page with articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80})
+    hash = Digest::MD5.hexdigest(user.email)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
+    image_tag(gravatar_url, alt: user.user_name, class: "rounded mx-auto d-block shadow mt-3")
+  end
 end
