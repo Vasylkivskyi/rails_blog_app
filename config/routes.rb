f51a4b39c73956bfe24e68354cbb1fed78384415
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2021-01-19 10:22:57 +0200

    add login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :articles  # Can be used like this also -->  resources :articles, only: [:show, :update, :destroy]
   get "signup", to: "users#new"
   resources :users, except: [:new]
+  # Authentication
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#delete"
 end
